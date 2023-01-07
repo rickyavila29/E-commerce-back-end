fd80b388e8c87cd69ef59c63735b95e4fd2c40b7
Ace <106693166+rickyavila29@users.noreply.github.com>
2023-01-07 11:45:12 -0700

    Added method to find all products and included category id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,6 +6,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    attributes: ["id","product_name","category_id"],
+    include: [{
+      model: Category,
+      attributes: ["id",'category_id'],
+    }]
+  })
   // be sure to include its associated Category and Tag data
 });
 
